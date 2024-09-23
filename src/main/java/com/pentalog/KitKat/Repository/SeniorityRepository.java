6ba47fd2806c003033d1ab7276e11a9c09c49f97
Berco Andrei <andrei2003berco@gmail.com>
2024-09-23 12:14:11 +0300

    fix seniority repository

--- src/main/java/com/pentalog/KitKat/Repository/SeniorityRepository.java
+++ src/main/java/com/pentalog/KitKat/Repository/SeniorityRepository.java
@@ -5,6 +5,8 @@ import com.pentalog.KitKat.Entities.Role;
 import com.pentalog.KitKat.Entities.Seniority;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SeniorityRepository extends JpaRepository<Seniority, Integer> {
+import java.util.Optional;
 
+public interface SeniorityRepository extends JpaRepository<Seniority, Integer> {
+    Optional<Seniority> findByName(String name);
 }
